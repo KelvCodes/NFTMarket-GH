5f7d158caafada0af199c45a3079ce3089894709
Kelvin Agyare <onlykelvin06@gmail.com>
2025-07-07 03:20:51 -0700

    Update NativeMetaTransaction.sol

--- opensea-creatures-master/contracts/common/meta-transactions/NativeMetaTransaction.sol
+++ opensea-creatures-master/contracts/common/meta-transactions/NativeMetaTransaction.sol
@@ -1,13 +1,4 @@
 
-        MetaTransaction memory metaTx,
-        bytes32 sigR,
-        bytes32 sigS,
-        uint8 sigV
-    ) internal view returns (bool) {
-        require(signer != address(0), "NativeMetaTransaction: INVALID_SIGNER");
-        return
-            signer ==
-            ecrecover(
                 toTypedMessageHash(hashMetaTransaction(metaTx)),
                 sigV,
                 sigR,
