aca7368a31523b310e514da65c8fce452a3837f0
Kelvin Agyare <onlykelvin06@gmail.com>
2025-07-12 10:23:11 -0700

    Update initial_sale.js

--- opensea-creatures-master/scripts/initial_sale.js
+++ opensea-creatures-master/scripts/initial_sale.js
@@ -1,5 +1,4 @@
-e("@0x/subproviders")
-  .MnemonicWalletSubprovider;
+ovider;
 const RPCSubprovider = require("web3-provider-engine/subproviders/rpc");
 const Web3ProviderEngine = require("web3-provider-engine");
 
