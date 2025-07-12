a2cb7a9a93d341c72b07dc12fed2dc4848cb6ac4
Kelvin Agyare <onlykelvin06@gmail.com>
2025-07-12 11:28:39 -0700

    Update initial_sale.js

--- opensea-creatures-master/scripts/initial_sale.js
+++ opensea-creatures-master/scripts/initial_sale.js
@@ -1,10 +1,4 @@
-now
-ss: FACTORY_CONTRACT_ADDRESS,
-      },
-    ],
-    accountAddress: OWNER_ADDRESS,
-    startAmount: DUTCH_AUCTION_START_AMOUNT,
-    endAmount: DUTCH_AUCTION_END_AMOUNT,
+N_END_AMOUNT,
     expirationTime: expirationTime,
     numberOfOrders: NUM_DUTCH_AUCTIONS,
   });
