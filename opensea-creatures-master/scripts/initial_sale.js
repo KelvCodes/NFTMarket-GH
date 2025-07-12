6f99d1ab544765a6db8bb460edf73446971505d1
Kelvin Agyare <onlykelvin06@gmail.com>
2025-07-12 11:23:47 -0700

    Update initial_sale.js

--- opensea-creatures-master/scripts/initial_sale.js
+++ opensea-creatures-master/scripts/initial_sale.js
@@ -1,6 +1,4 @@
 
-const BASE_DERIVATION_PATH = `44'/60'/0'/0`;
-
 const mnemonicWalletSubprovider = new MnemonicWalletSubprovider({
   mnemonic: MNEMONIC,
   baseDerivationPath: BASE_DERIVATION_PATH,
