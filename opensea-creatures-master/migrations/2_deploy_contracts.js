14cc990e04d7d8188a8a61b3e25c0cb9df8e8403
Kelvin Agyare <onlykelvin06@gmail.com>
2025-07-13 07:49:32 -0700

    Update 2_deploy_contracts.js

--- opensea-creatures-master/migrations/2_deploy_contracts.js
+++ opensea-creatures-master/migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 
-("./CreatureFactory.sol");
+
 const CreatureLootBox = artifacts.require("./CreatureLootBox.sol");
 const CreatureAccessory = artifacts.require("../contracts/CreatureAccessory.sol");
 const CreatureAccessoryFactory = artifacts.require("../contracts/CreatureAccessoryFactory.sol");
