f5bdca87faf7ddb77766a937990e77ae68910d3c
Kelvin Agyare <onlykelvin06@gmail.com>
2025-07-15 15:03:47 -0700

    Update initial_sale.js

--- opensea-creatures-master/scripts/initial_sale.js
+++ opensea-creatures-master/scripts/initial_sale.js
@@ -1,4 +1,4 @@
-d environment variables are set
+
 if (!MNEMONIC || !NODE_API_KEY || !NETWORK || !OWNER_ADDRESS) {
   console.error(
     "Please set a mnemonic, Alchemy/Infura key, owner, network, API key, nft contract, and factory contract address."
