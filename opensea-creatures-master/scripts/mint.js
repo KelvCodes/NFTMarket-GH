7c723ab7d4f8d118bc22a017efc2ec4d2b5a2130
Kelvin Agyare <onlykelvin06@gmail.com>
2025-07-17 14:32:15 -0700

    Update mint.js

--- opensea-creatures-master/scripts/mint.js
+++ opensea-creatures-master/scripts/mint.js
@@ -1,5 +1,4 @@
-s.env.ALCHEMY_KEY;
-const isInfura = !!process.env.INFURA_KEY;
+ess.env.INFURA_KEY;
 const FACTORY_CONTRACT_ADDRESS = process.env.FACTORY_CONTRACT_ADDRESS;
 const NFT_CONTRACT_ADDRESS = process.env.NFT_CONTRACT_ADDRESS;
 const OWNER_ADDRESS = process.env.OWNER_ADDRESS;
