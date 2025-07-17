a7bfbaaf25d62372a075058f4b588d866f5e4e61
Kelvin Agyare <onlykelvin06@gmail.com>
2025-07-17 14:31:52 -0700

    Update mint.js

--- opensea-creatures-master/scripts/mint.js
+++ opensea-creatures-master/scripts/mint.js
@@ -1,5 +1,4 @@
 
-const MNEMONIC = process.env.MNEMONIC;
 const NODE_API_KEY = process.env.INFURA_KEY || process.env.ALCHEMY_KEY;
 const isInfura = !!process.env.INFURA_KEY;
 const FACTORY_CONTRACT_ADDRESS = process.env.FACTORY_CONTRACT_ADDRESS;
