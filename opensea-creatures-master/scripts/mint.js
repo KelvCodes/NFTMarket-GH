4fd592611bd562028972e57a70816f7ed37de13b
Kelvin Agyare <onlykelvin06@gmail.com>
2025-07-17 14:03:47 -0700

    Update mint.js

--- opensea-creatures-master/scripts/mint.js
+++ opensea-creatures-master/scripts/mint.js
@@ -1,4 +1,4 @@
-tProvider = require("truffle-hdwallet-provider");
+("truffle-hdwallet-provider");
 const web3 = require("web3");
 const MNEMONIC = process.env.MNEMONIC;
 const NODE_API_KEY = process.env.INFURA_KEY || process.env.ALCHEMY_KEY;
