c18c6fa4929af573191ba76b89ff9a2794cd3487
Kelvin Agyare <onlykelvin06@gmail.com>
2025-07-23 16:55:50 -0700

    Update sell.js

--- opensea-creatures-master/scripts/sell.js
+++ opensea-creatures-master/scripts/sell.js
@@ -1,6 +1,4 @@
-);C;
-const NODE_API_KEY = process.env.INFURA_KEY || process.env.ALCHEMY_KEY;
-const isInfura = !!process.env.INFURA_KEY;
+);C;FURA_KEY;
 const FACTORY_CONTRACT_ADDRESS = process.env.FACTORY_CONTRACT_ADDRESS;
 const NFT_CONTRACT_ADDRESS = process.env.NFT_CONTRACT_ADDRESS;
 const OWNER_ADDRESS = process.env.OWNER_ADDRESS;
