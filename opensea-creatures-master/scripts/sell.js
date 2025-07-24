bf63aec4931fd5afe59624eee1651df2f8495d62
Kelvin Agyare <onlykelvin06@gmail.com>
2025-07-23 17:01:01 -0700

    Update sell.js

--- opensea-creatures-master/scripts/sell.js
+++ opensea-creatures-master/scripts/sell.js
@@ -1,14 +1,1 @@
-ated a dutch auction sell order! ${dutchAuctionSellOrder.asset.openseaLink}\n`
-  );cd5ab";
-  const englishAuctionSel
-    expirationTime: expirationTime,
-    waitForHighestBid: true,
-    paymentTokenAddress: wethAddress,
-    accountAddress: OWNER_ADDRESS,
-  });
-  console.log(
-    `Successfully created an English auction sell order! ${englishAuctionSellOrder.asset.openseaLink}\n`
-  );
-}
 
-main();
