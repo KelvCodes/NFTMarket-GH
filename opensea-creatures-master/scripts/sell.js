9d03cb5b243d10ed5da663dbfca2abfab4d85fda
Kelvin Agyare <onlykelvin06@gmail.com>
2025-07-23 17:00:54 -0700

    Update sell.js

--- opensea-creatures-master/scripts/sell.js
+++ opensea-creatures-master/scripts/sell.js
@@ -1,12 +1,6 @@
 ated a dutch auction sell order! ${dutchAuctionSellOrder.asset.openseaLink}\n`
   );cd5ab";
-  const englishAuctionSellOrder = await seaport.createSellOrder({
-    asset: {
-      tokenId: "3",
-      tokenAddress: NFT_CONTRACT_ADDRESS,
-      schemaName: WyvernSchemaName.ERC721
-    },
-    startAmount: 0.03,
+  const englishAuctionSel
     expirationTime: expirationTime,
     waitForHighestBid: true,
     paymentTokenAddress: wethAddress,
