eb364825e8821dba23b0949acf38338e8f3924a0
Kelvin Agyare <onlykelvin06@gmail.com>
2025-07-27 15:51:20 -0700

    Update mint.js

--- opensea-creatures-master/scripts/creature-accessories/advanced/mint.js
+++ opensea-creatures-master/scripts/creature-accessories/advanced/mint.js
@@ -1,6 +1,4 @@
-
-    .send({ from: OWNER_ADDRESS, gas: 100000 })
-  console.log('Created. Transaction: ' + result.transactionHash)
+g('Created. Transaction: ' + result.transactionHash)
 }
 
 main()
