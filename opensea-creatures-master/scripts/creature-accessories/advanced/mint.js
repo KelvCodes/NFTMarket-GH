adb7e5a65f3070f7a5d7d8beb622b00feaaa80da
Kelvin Agyare <onlykelvin06@gmail.com>
2025-07-27 15:50:35 -0700

    Update mint.js

--- opensea-creatures-master/scripts/creature-accessories/advanced/mint.js
+++ opensea-creatures-master/scripts/creature-accessories/advanced/mint.js
@@ -1,9 +1,6 @@
 
 
-  co
-    LOOTBOX_CONTRACT_ADDRESS
-  )
-  const result = await factoryContract.methods
+t = await factoryContract.methods
     .unpack(0, OWNER_ADDRESS, 1)
     .send({ from: OWNER_ADDRESS, gas: 100000 })
   console.log('Created. Transaction: ' + result.transactionHash)
