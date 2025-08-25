4f928d1d0d4b5c0676ae3685b5c843fd8f2fb030
Kelvin Agyare <onlykelvin06@gmail.com>
2025-08-25 10:47:55 -0700

    Update truffle.js

--- opensea-creatures-master/truffle.js
+++ opensea-creatures-master/truffle.js
@@ -1,4 +1,4 @@
-Determine if the current npm script requires a connection to a remote node (Rinkeby or Mainnet)
+ a connection to a remote node (Rinkeby or Mainnet)
 const needsNodeAPI =
   process.env.npm_config_argv &&
   (process.env.npm_config_argv.includes("rinkeby") ||
