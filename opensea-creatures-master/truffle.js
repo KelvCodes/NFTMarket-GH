ddbea3d27ef948639d37012f38b689af72f4b4e1
Kelvin Agyare <onlykelvin06@gmail.com>
2025-08-25 10:57:57 -0700

    Update truffle.js

--- opensea-creatures-master/truffle.js
+++ opensea-creatures-master/truffle.js
@@ -2,15 +2,7 @@
     reporter: "eth-gas-reporter", // Report gas usage per test
     reporterOptions: {
       currency: "USD",           // Show cost in USD
-      gas
-    solc: {
-      version: "^0.8.0",         // Use Solidity v0.8.x and above
-      settings: {
-        optimizer: {
-          enabled: true,         // Enable optimizer for smaller contract size
-          runs: 20,              // Optimize for contracts that will be run ~20 times
-        },
-      },
+
     },
   },
 
