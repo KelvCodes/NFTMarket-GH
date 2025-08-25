eac70a7175cf1b0a88155ba5c535348195923013
Kelvin Agyare <onlykelvin06@gmail.com>
2025-08-25 10:58:02 -0700

    Update truffle.js

--- opensea-creatures-master/truffle.js
+++ opensea-creatures-master/truffle.js
@@ -6,14 +6,7 @@
     },
   },
 
-  // Truffle plugins
-  plugins: [
-    'truffle-plugin-verify',     // Plugin to verify contracts on Etherscan
-  ],
-
-  // API keys used for Etherscan contract verification
-  api_keys: {
-    etherscan: 'ETHERSCAN_API_KEY_FOR_VERIFICATION', // Replace with your actual Etherscan API key
+  // Truffle pOR_VERIFICATION', // Replace with your actual Etherscan API key
   }
 };
 
