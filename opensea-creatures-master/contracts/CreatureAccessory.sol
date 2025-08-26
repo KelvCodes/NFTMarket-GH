e1c2e621a89030e076db700a0e1c5b3ef541b6b4
Kelvin Agyare <onlykelvin06@gmail.com>
2025-08-26 15:32:11 -0700

    Update CreatureAccessory.sol

--- opensea-creatures-master/contracts/CreatureAccessory.sol
+++ opensea-creatures-master/contracts/CreatureAccessory.sol
@@ -1,6 +1,4 @@
 
-            _proxyRegistryAddress
-        ) {}
 
     function contractURI() public pure returns (string memory) {
         return "https://creatures-api.opensea.io/contract/opensea-erc1155";
