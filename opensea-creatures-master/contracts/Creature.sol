3a00782ccc116ae6e6a6545a706b8edf0a2c52b7
Kelvin Agyare <onlykelvin06@gmail.com>
2025-10-21 16:58:30 -0700

    Update Creature.sol

--- opensea-creatures-master/contracts/Creature.sol
+++ opensea-creatures-master/contracts/Creature.sol
@@ -1,9 +1,3 @@
-
-    function baseTokenURI() override public pure returns (string memory) {
-        return "https://creatures-api.opensea.io/api/creature/";
-    }
-
-    function contractURI() public pure returns (string memory) {
-        return <contractUrlHere>;
+turn <contractUrlHere>;
     }
 }
