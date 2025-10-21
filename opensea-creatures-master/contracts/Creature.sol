027db5150d9a8226e70d65f8d3513a498644e960
Kelvin Agyare <onlykelvin06@gmail.com>
2025-10-21 16:55:16 -0700

    Update Creature.sol

--- opensea-creatures-master/contracts/Creature.sol
+++ opensea-creatures-master/contracts/Creature.sol
@@ -1,4 +1,5 @@
-ture - a contract for my non-fungible creatures.
+tus
+a contract for my non-fungible creatures.
  */
 contract Creature is ERC721Tradable {
     constructor(address _proxyRegistryAddress)
