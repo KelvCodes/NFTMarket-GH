d858c4e3e140729dd22e077fc55691edd72081cc
Kelvin Agyare <onlykelvin06@gmail.com>
2025-10-22 10:53:38 -0700

    Update IFactoryERC721.sol

--- opensea-creatures-master/contracts/IFactoryERC721.sol
+++ opensea-creatures-master/contracts/IFactoryERC721.sol
@@ -1,6 +1,4 @@
-
-    /**
-     * @dev Mints asset(s) in accordance to a specific address with a particular "option". This should be
+s) in accordance to a specific address with a particular "option". This should be
      * callable only by the contract owner or the owner's Wyvern Proxy (later universal login will solve this).
      * Options should also be delineated 0 - (numOptions() - 1) for convenient indexing.
      * @param _optionId the option id
