24540135403d13770b3eec6448c14619afd964d6
Kelvin Agyare <onlykelvin06@gmail.com>
2025-10-22 10:54:46 -0700

    Update IFactoryERC721.sol

--- opensea-creatures-master/contracts/IFactoryERC721.sol
+++ opensea-creatures-master/contracts/IFactoryERC721.sol
@@ -1,8 +1,1 @@
 
-     * Options should also be delineated 0 - (numOptions() - 1) for convenient indexing.
-     * @param _optionId the option id
-     * @param _toAddress address of the future owner of the asset(s)
-     */
-    function mint(uint256 _optionId, address _toAddress) external;
-}
-
