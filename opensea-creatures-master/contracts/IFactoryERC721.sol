0cf8454fe050ff093005ad5394ef0154de3f6b6a
Kelvin Agyare <onlykelvin06@gmail.com>
2025-10-22 01:21:30 -0700

    Update IFactoryERC721.sol

--- opensea-creatures-master/contracts/IFactoryERC721.sol
+++ opensea-creatures-master/contracts/IFactoryERC721.sol
@@ -1,6 +1,5 @@
 
-/**
- * This is a generic factory contract that can be used to mint tokens. The configuration
+is is a generic factory contract that can be used to mint tokens. The configuration
  * for minting is specified by an _optionId, which can be used to delineate various
  * ways of minting.
  */
