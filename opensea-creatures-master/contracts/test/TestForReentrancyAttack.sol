1bf07044d3a6ff938403d7b15cec2b31d2bb6f1c
Kelvin Agyare <onlykelvin06@gmail.com>
2025-10-23 09:38:13 -0700

    Update TestForReentrancyAttack.sol

--- opensea-creatures-master/contracts/test/TestForReentrancyAttack.sol
+++ opensea-creatures-master/contracts/test/TestForReentrancyAttack.sol
@@ -1,5 +1,4 @@
-ERC1155Received(address,address,uint256,uint256,bytes)"))
-    bytes4 constant internal ERC1155_RECEIVED_SIG = 0xf23a6e61;
+constant internal ERC1155_RECEIVED_SIG = 0xf23a6e61;
     // bytes4(keccak256("onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)"))
     bytes4 constant internal ERC1155_BATCH_RECEIVED_SIG = 0xbc197c81;
     // bytes4(keccak256("onERC1155Received(address,address,uint256,uint256,bytes)")) ^ bytes4(keccak256("onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)"))
