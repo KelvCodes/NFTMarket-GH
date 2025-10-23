7187426c0d88fc62f2969224bad8da794ac69db5
Kelvin Agyare <onlykelvin06@gmail.com>
2025-10-23 10:54:10 -0700

    Update TestForReentrancyAttack.sol

--- opensea-creatures-master/contracts/test/TestForReentrancyAttack.sol
+++ opensea-creatures-master/contracts/test/TestForReentrancyAttack.sol
@@ -1,6 +1,6 @@
 
 
-    /*function attack(uint256 _totalToMint) external {
+    /*futtack(uint256 _totalToMint) external {
         require(_totalToMint >= 2, "_totalToMint must be >= 2");
         totalToMint = _totalToMint;
         CreatureAccessoryFactory(factoryAddress).mint(1, address(this), 1, "");
