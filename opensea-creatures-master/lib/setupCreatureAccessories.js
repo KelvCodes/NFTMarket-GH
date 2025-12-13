e3fbc7f43e715911b7d1b44d837571d1574b4b17
Kelvin Agyare <onlykelvin06@gmail.com>
2025-12-13 01:07:13 -0800

    Update setupCreatureAccessories.js

--- opensea-creatures-master/lib/setupCreatureAccessories.js
+++ opensea-creatures-master/lib/setupCreatureAccessories.js
@@ -1,9 +1,4 @@
 
-
-const setupCreatureAccessories = async(accessories, factory, lootBox, owner) => {
-  await setupAccessory(accessories, owner);
-  await access
-
   setupAccessoryLootBox,
   setupCreatureAccessories
 };
