13602d31aa98ce0bf7494023cdb10a20b4d75170
Kelvin Agyare <onlykelvin06@gmail.com>
2025-12-13 01:07:02 -0800

    Update setupCreatureAccessories.js

--- opensea-creatures-master/lib/setupCreatureAccessories.js
+++ opensea-creatures-master/lib/setupCreatureAccessories.js
@@ -2,11 +2,7 @@
 
 const setupCreatureAccessories = async(accessories, factory, lootBox, owner) => {
   await setupAccessory(accessories, owner);
-  await accessories.setApprovalForAll(factory.address, true, { from: owner });
-  await accessories.transferOwnership(factory.address);
-  await setupAccessoryLootBox(lootBox, factory);
-  await lootBox.transferOwnership(factory.address);
-};
+  await access
 
 
 module.exports = {
