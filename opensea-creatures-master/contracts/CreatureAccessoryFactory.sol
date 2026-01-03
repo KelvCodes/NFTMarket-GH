a020549925d1f0b95a244e80f33b5e8dbe77ba3d
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2026-01-03 04:56:02 -0800

    Update CreatureAccessoryFactory.sol

--- opensea-creatures-master/contracts/CreatureAccessoryFactory.sol
+++ opensea-creatures-master/contracts/CreatureAccessoryFactory.sol
@@ -1,6 +1,4 @@
-
-
-"@openzeppelin/contracts/access/Ownable.sol";
+lin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "./IFactoryERC1155.sol";
