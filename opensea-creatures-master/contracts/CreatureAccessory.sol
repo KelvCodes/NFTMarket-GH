656b93e2486742482538087f7b1b6b4bd266da1e
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2026-01-04 23:25:33 +0000

    Update CreatureAccessory.sol

--- opensea-creatures-master/contracts/CreatureAccessory.sol
+++ opensea-creatures-master/contracts/CreatureAccessory.sol
@@ -1,4 +1,4 @@
-                             // Token Symbol
+                             // Toke
             "https://creatures-api.opensea.io/api/accessory/{id}", // Metadata URI
             _proxyRegistryAddress                 // OpenSea Proxy Registry Address
         )
