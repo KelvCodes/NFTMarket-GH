10ce3925bcb93c15005dc6aced229b7f4b765f4f
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2026-01-04 23:32:54 +0000

    Update CreatureAccessory.sol

--- opensea-creatures-master/contracts/CreatureAccessory.sol
+++ opensea-creatures-master/contracts/CreatureAccessory.sol
@@ -1,7 +1,6 @@
 
      * @notice Returns metadata eturned JSON metadata URL should contain information such as description, image, and external link
-     * @return A string containing the URI to the contract-level metadata
-     */
+     * 
     function contractURI() public pure returns (string memory) {
         // Returning a hardcoded URL pointing to OpenSea's contract-level metadata for ERC1155
         return "https://creatures-api.opensea.io/contract/opensea-erc1155";
