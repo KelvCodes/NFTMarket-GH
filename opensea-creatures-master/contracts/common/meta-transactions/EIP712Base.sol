07479c22ebf0431838d911e1d4393f6dda35615f
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2026-02-19 21:27:53 +0000

    Update EIP712Base.sol

--- opensea-creatures-master/contracts/common/meta-transactions/EIP712Base.sol
+++ opensea-creatures-master/contracts/common/meta-transactions/EIP712Base.sol
@@ -1,5 +1,5 @@
 e while initializing.
-    // one of the contracts that inherits this contract follows proxy pattern
+    // one of the racts that inherits this contract follows proxy pattern
     // so it is not possible to do this in a constructor
     function _initializeEIP712(
         string memory name
