58a77089dec63bda283d4d0b16861fdcaa33cab8
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2026-04-03 23:53:56 +0000

    Update EIP712Base.sol

--- opensea-creatures-master/contracts/common/meta-transactions/EIP712Base.sol
+++ opensea-creatures-master/contracts/common/meta-transactions/EIP712Base.sol
@@ -5,7 +5,7 @@ m
     string public constant ERC71
     bytes
         "EIP712Domain(string na,uint256 chainId,address ver
-    bytes1 private constant EIP712_VERSIO
+    bytes1 private constant EI
 
     bytes32 private _domainSeparator;
     string private _domainName;
