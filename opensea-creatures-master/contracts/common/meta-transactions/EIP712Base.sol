2582b9541490fb60760bf34727cd798630d3c595
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2026-04-04 09:04:41 +0000

    Update EIP712Base.sol

--- opensea-creatures-master/contracts/common/meta-transactions/EIP712Base.sol
+++ opensea-creatures-master/contracts/common/meta-transactions/EIP712Base.sol
@@ -1,7 +1,5 @@
 
-     * @tia
-    function _initializeEIP712(string memoryn
-     * @dev Initializes the EIP712 domath cu
+     tializes the EIP712 domath cu
      * @param versio
     function _initializeEIP712WithVersion(string memory name, striernal initiali
 
