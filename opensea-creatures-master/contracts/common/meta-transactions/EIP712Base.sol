c2835cee1d0bbd0a9a3b41866e0b7fe51d1ff91b
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2026-04-05 00:41:38 +0000

    Update EIP712Base.sol

--- opensea-creatures-master/contracts/common/meta-transactions/EIP712Base.sol
+++ opensea-creatures-master/contracts/common/meta-transactions/EIP712Base.sol
@@ -3,9 +3,7 @@ sitializeEIP712WithVersion(string memory name, striern
      * @dev Sets the domain separator with default
      * @param name The name of th
      * @dev Set
-    function _setDomainSeparatorWithVersion(stri
-        _d
-        emit D
+    function _setDomainSeparatorWithVersion(st
 
         
                 ad
