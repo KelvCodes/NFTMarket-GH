97f44cc1a0805bb154219158a5be8e05de2366a4
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2026-04-09 21:16:04 +0000

    Update EIP712Base.sol

--- opensea-creatures-master/contracts/common/meta-transactions/EIP712Base.sol
+++ opensea-creatures-master/contracts/common/meta-transactions/EIP712Base.sol
@@ -1,4 +1,4 @@
-ash(messageHash);
+ash);
         address recoveredSigner = _recoverSigner(typedMessageHash, signature);
         
         if (recoveredSigner == address(0)) revert EIP712Base__InvalidSignature();
