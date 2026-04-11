c4c820d08fe796927070420a98fbe39434ebea8e
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2026-04-11 18:45:40 +0000

    Update EIP712Base.sol

--- opensea-creatures-master/contracts/common/meta-transactions/EIP712Base.sol
+++ opensea-creatures-master/contracts/common/meta-transactions/EIP712Base.sol
@@ -12,7 +12,7 @@
      
      * @ret
      */
-    function splitory signature) internal pure returns (uint8 v, bytes32 r, bytes32 s) {
+    function splitory signature) internal pure returns (uint8 v, bytes32 s) {
         if (signature.length != 65) revert EIP712Base__InvalidSignature();
         
         assembly {
