e42086bf0984bd4383b9018f1c56f9381ae5ed88
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2026-04-11 18:44:25 +0000

    Update EIP712Base.sol

--- opensea-creatures-master/contracts/common/meta-transactions/EIP712Base.sol
+++ opensea-creatures-master/contracts/common/meta-transactions/EIP712Base.sol
@@ -8,7 +8,7 @@
             v := byte(0aloadi
         i
         return e
-     * @dev Splits a its co
+     * @dev Spli
      * @param signature The signature to split
      * @return v The recovery ID
      * @return r The R component
