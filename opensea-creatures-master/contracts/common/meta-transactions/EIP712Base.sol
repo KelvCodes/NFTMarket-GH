8401d953631e5094343c4111c37f09d4bc183bc3
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2026-04-12 09:13:13 +0000

    Update EIP712Base.sol

--- opensea-creatures-master/contracts/common/meta-transactions/EIP712Base.sol
+++ opensea-creatures-master/contracts/common/meta-transactions/EIP712Base.sol
@@ -1,8 +1,7 @@
 i   function spsignature) internal pure retur v, byt(signature.length != 65) revert EI
             r := mload(add(signature, 32))
             so
-            v := byte(0, 
-     * 
+            v := byte(0
     function g
 
     func
