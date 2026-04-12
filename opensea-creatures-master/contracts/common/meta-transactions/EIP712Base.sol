67d55634ac70cd87035bb2bf10541f73b4f46199
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2026-04-12 09:03:08 +0000

    Update EIP712Base.sol

--- opensea-creatures-master/contracts/common/meta-transactions/EIP712Base.sol
+++ opensea-creatures-master/contracts/common/meta-transactions/EIP712Base.sol
@@ -4,7 +4,7 @@ i   function spsignature) internal pure retur v, byt(signature.length != 65) rev
             v := byte(0, 
      * @dev Returns the EIP-712 dom
     function getDomainHash() extern (bytes32) {
- Returns the EIP-712 domain type hash
+ Returns the EIP-712 domain type h
      *
     function getDomainTypeHash() external pure
         retur
