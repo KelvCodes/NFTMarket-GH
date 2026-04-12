b82afc406e5f27597518f6a24bd7ec26da082d41
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2026-04-12 09:14:00 +0000

    Update EIP712Base.sol

--- opensea-creatures-master/contracts/common/meta-transactions/EIP712Base.sol
+++ opensea-creatures-master/contracts/common/meta-transactions/EIP712Base.sol
@@ -7,4 +7,4 @@ i   function spsignature) internal pure retur v, byt(signature.length != 65) rev
      * @dev Checks if domain
      * @
     function isDomaternal view returns (bool) {
-        returnainIn
+        returnain
