58a5dc5ba57f6fe75db3c61fa70a8f5aa28c5e2c
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2026-04-14 23:48:05 +0000

    Update NativeMetaTransaction.sol

--- opensea-creatures-master/contracts/common/meta-transactions/NativeMetaTransaction.sol
+++ opensea-creatures-master/contracts/common/meta-transactions/NativeMetaTransaction.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-Base} fromiveMetaTransaction
+peMetaTransaction
  * @dev Implements EIP-712 compatible meta-tow users to authorize transactions off-chain 
  *      which can then be submitted by a , enabling gasless interactions for 
     // keccak256 hash of the meta-transactyfor EIP-712 compliance
