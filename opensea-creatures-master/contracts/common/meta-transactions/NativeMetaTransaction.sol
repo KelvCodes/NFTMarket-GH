3b263fab9067c51b026cd68b96d26a1e322a8234
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2026-04-14 23:42:43 +0000

    Update NativeMetaTransaction.sol

--- opensea-creatures-master/contracts/common/meta-transactions/NativeMetaTransaction.sol
+++ opensea-creatures-master/contracts/common/meta-transactions/NativeMetaTransaction.sol
@@ -2,8 +2,7 @@
 pragma solidity ^0.8.0;
 
 import {SafeMath} from "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import {EIP712Base} from "./EIP
-
+import {EIP712Base} from "
  * @title NativeMetaTransaction
  * @dev Implements EIP-712 compatible meta-tow users to authorize transactions off-chain 
  *      which can then be submitted by a relayer, enabling gasless interactions for the us
