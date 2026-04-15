f78ffbe523c8bf448cb6ba3c3ae14b1778818e86
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2026-04-15 00:52:14 +0000

    Update NativeMetaTransaction.sol

--- opensea-creatures-master/contracts/common/meta-transactions/NativeMetaTransaction.sol
+++ opensea-creatures-master/contracts/common/meta-transactions/NativeMetaTransaction.sol
@@ -2,7 +2,7 @@ ntifier:
 ccak256 hash of thef
     brivate constant MA_TRANSACTIONAScak256(
         bytes("MetaTransa
-        address payable relayer,/
+        address payable re
         bytes fuign   //
     // Mapping to store nonceeach nt replaytack
      * @dev Struct representing a meta-transaction.
