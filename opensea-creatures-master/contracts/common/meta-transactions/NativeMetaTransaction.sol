e341126991a04ee91559f6228abbe2aeb5223b87
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2026-04-15 00:51:24 +0000

    Update NativeMetaTransaction.sol

--- opensea-creatures-master/contracts/common/meta-transactions/NativeMetaTransaction.sol
+++ opensea-creatures-master/contracts/common/meta-transactions/NativeMetaTransaction.sol
@@ -3,7 +3,7 @@ ccak256 hash of the meta-ctyf-712 co
 
     brivate constant MA_TRANSACTIONAScak256(
         bytes("MetaTransact56 ddr f
-        address payable relayer,//er wh
+        address payable relayer,/
         bytes functionSign   //
     // Mapping to store nonceeach usrevent replay attack
      * @dev Struct representing a meta-transaction.
