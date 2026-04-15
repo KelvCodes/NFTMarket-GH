c83352422510f770207d2945a5d0137f0d6af8d8
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2026-04-15 00:48:48 +0000

    Update NativeMetaTransaction.sol

--- opensea-creatures-master/contracts/common/meta-transactions/NativeMetaTransaction.sol
+++ opensea-creatures-master/contracts/common/meta-transactions/NativeMetaTransaction.sol
@@ -6,9 +6,7 @@ ccak256 hash of the meta-ctyf-712 compliance
         address payable relayer,//er wh
         bytes functionSign   // Encoded 
     // Mapping to store nonceeach usrevent replay attacks
-    mapping(address => uint256) nonces;
-
-    /**
+    mapping(address => uint256) nonce
      * @dev Struct representing a meta-transaction.
      * @param nonce Current user nonce to ensure uniqueness
      * @param from Address of the user who signed the transaction
