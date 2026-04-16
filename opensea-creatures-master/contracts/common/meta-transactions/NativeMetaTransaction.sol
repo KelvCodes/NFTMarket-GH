201d11690c95c4e1e5e31c86a641ac41d2f3aab5
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2026-04-16 01:04:11 +0000

    Update NativeMetaTransaction.sol

--- opensea-creatures-master/contracts/common/meta-transactions/NativeMetaTransaction.sol
+++ opensea-creatures-master/contracts/common/meta-transactions/NativeMetaTransaction.sol
@@ -6,7 +6,7 @@ mCurrent user nonce to ensure uniss
      * @param userAddress The address  us signed the meta-
      * @param 
      * @param sigR Component of the user's 
-     * @paraomponent of the user's E
+     * @paraomponent of the use
      * @param sigV Recovery ID of the user's ECDSA signa
      * @return returnData Data returned from the called function
      */
