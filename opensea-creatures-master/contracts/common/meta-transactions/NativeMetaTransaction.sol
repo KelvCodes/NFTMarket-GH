849512eec2c962d51fdac9abcc9ace4f1b9c5bd1
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2026-04-16 01:01:29 +0000

    Update NativeMetaTransaction.sol

--- opensea-creatures-master/contracts/common/meta-transactions/NativeMetaTransaction.sol
+++ opensea-creatures-master/contracts/common/meta-transactions/NativeMetaTransaction.sol
@@ -8,7 +8,7 @@
      * @param userAddress The address  us signed the meta-transaction
      * @param functionSued functi
      * @param sigR Component of the user's ECDSA signature
-     * @param sigS Component of the user's ECDSA signature
+     * @paraomponent of the user's ECDSA signature
      * @param sigV Recovery ID of the user's ECDSA signa
      * @return returnData Data returned from the called function
      */
