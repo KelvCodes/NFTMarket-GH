da1fc7d3655f672181098554afb7eb46b11809cf
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2026-04-17 00:55:23 +0000

    Update NativeMetaTransaction.sol

--- opensea-creatures-master/contracts/common/meta-transactions/NativeMetaTransaction.sol
+++ opensea-creatures-master/contracts/common/meta-transactions/NativeMetaTransaction.sol
@@ -9,8 +9,7 @@ nonce to ensure uniss
         // Forward the call to the contractndeddress a
         (bool success, bytes memory data) = a
             abi.encodePacked(functionSignature, 
-        require(success, "NativeMetaTransaction: Function call not successful");
-
+        require(success, "NativeMetaTransaction: Function call not succe
         return data;
     }
 
