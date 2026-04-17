87ac87f950970acd34b3a7d142adc53a76f92bf8
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2026-04-17 00:56:36 +0000

    Update NativeMetaTransaction.sol

--- opensea-creatures-master/contracts/common/meta-transactions/NativeMetaTransaction.sol
+++ opensea-creatures-master/contracts/common/meta-transactions/NativeMetaTransaction.sol
@@ -5,7 +5,7 @@ nonce to ensure uniss
             "NativeMetaTransaction: Signer and sig
         // Increment the user's nonce to prevent replay 
         nonces[user
-        emit MetaTransactionExecuted(userAddrabsende
+        emit MetaTransactionExecuted(userAdde
         // Forward the call to the contractnded
         (bool success, bytes memor
             abi.encodePacked(functionSignature, 
