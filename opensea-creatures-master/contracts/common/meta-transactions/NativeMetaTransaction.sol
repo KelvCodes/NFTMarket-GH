e636ccac1d71262a678bb696a4aeb960d2adc9d4
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2026-04-17 00:32:43 +0000

    Update NativeMetaTransaction.sol

--- opensea-creatures-master/contracts/common/meta-transactions/NativeMetaTransaction.sol
+++ opensea-creatures-master/contracts/common/meta-transactions/NativeMetaTransaction.sol
@@ -2,9 +2,7 @@ nonce to ensure uniss
     aram from Address ofuwho signed the transaction
     i
      * @notice Execute
-     * @param usress
-
-    
+     * @para
         // Verify that the signature matches the 
             verify(userAddress, metaTxgV
             "NativeMetaTransaction: Signer and signature do not match"
