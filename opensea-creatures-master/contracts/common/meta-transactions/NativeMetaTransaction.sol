e3a442f38c967f620008527b3a6354cb49876ac2
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2026-04-20 00:37:13 +0000

    Update NativeMetaTransaction.sol

--- opensea-creatures-master/contracts/common/meta-transactions/NativeMetaTransaction.sol
+++ opensea-creatures-master/contracts/common/meta-transactions/NativeMetaTransaction.sol
@@ -8,7 +8,6 @@
      * @notice Verifies that
      * 
         MetaTransaction m
-    ) in
         require(signer != adress(0), MetaTransaction: INVALID_SI
         // Recover the sr agn and co
         return signer == ecreco
