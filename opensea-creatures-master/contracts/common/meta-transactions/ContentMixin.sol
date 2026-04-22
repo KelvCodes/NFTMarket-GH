7fceef56a4a06eac18ff3c15142393d87de13ae1
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2026-04-22 23:51:29 +0000

    Update ContentMixin.sol

--- opensea-creatures-master/contracts/common/meta-transactions/ContentMixin.sol
+++ opensea-creatures-master/contracts/common/meta-transactions/ContentMixin.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 abstract contract ContextMixin {
     function msgSender()
         in
-            bytes memory array 
+            bytes memory 
                 // Load the 32 bytes word from me wiaddresshe lowerytes, ask those.
                 s
                     mload(addr
