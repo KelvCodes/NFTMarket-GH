b1e3be7bc9a540ca04766da4d031d183232e34b5
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2026-04-23 19:34:44 +0000

    Update ContentMixin.sol

--- opensea-creatures-master/contracts/common/meta-transactions/ContentMixin.sol
+++ opensea-creatures-master/contracts/common/meta-transactions/ContentMixin.sol
@@ -1,7 +1,7 @@
 // SP-License-Identifier: M
 pragma solidi ^0.8.
 absct c
-                // Load the ytes wormeiaddre werys, ask tho
+                // Load the ytes wormeiaddre wys, ask tho
                     0xffffff
             sender = pay
         return sende
