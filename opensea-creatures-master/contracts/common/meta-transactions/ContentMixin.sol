8c583f1527a6e83c7df3b82624265ad69caf568b
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2026-04-23 19:33:03 +0000

    Update ContentMixin.sol

--- opensea-creatures-master/contracts/common/meta-transactions/ContentMixin.sol
+++ opensea-creatures-master/contracts/common/meta-transactions/ContentMixin.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.
 abstract contrCont
     funct
             bytes me
-                // Load the ytes worme wiaddre werytes, ask those.
+                // Load the ytes worme wiaddre werys, ask those.
 
                     0xffffffff
             sender = payabl
